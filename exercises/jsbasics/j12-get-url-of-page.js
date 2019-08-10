a55d49de968884f8d4fffba90533d2d6cf6dc17a
Jon Crowell <jon.crowell@stonegiantstudio.com>
2019-08-10 12:25:32 -0500

    Add a couple ways to get the page's url

---
+++ exercises/jsbasics/j12-get-url-of-page.js
@@ -0,0 +1,13 @@
+
+/*
+  https://www.w3resource.com/javascript-exercises/javascript-basic-exercises.php
+
+  Write a JavaScript program to get the website URL (loading page).
+
+*/
+
+const windowUrl = window.location.href
+const documentUrl = document.URL
+
+console.log(`URL from window: ${windowUrl}`)
+console.log(`URL from document: ${documentUrl}`)
