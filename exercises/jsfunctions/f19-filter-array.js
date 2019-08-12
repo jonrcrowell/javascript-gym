6253ed0762ae4eb2f1bda3ff9fd4819b7139bc8e
Jon Crowell <jon.crowell@stonegiantstudio.com>
2019-08-12 08:51:55 -0500

    Add filter

---
+++ exercises/jsfunctions/f19-filter-array.js
@@ -0,0 +1,22 @@
+// use Quokka to see results in VS Code
+
+/* 
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof    
+
+https://www.w3resource.com/javascript-exercises/javascript-functions-exercises.php
+    
+    Write a JavaScript function that returns array elements larger than a number.
+    
+    [0, 1, 3, 17, 45, 93, 91, 128, 8]
+    [0, 1, 3, 17, 45, 93, 91, 128, 8, 0, 1, 3, 17, 45, 93, 91, 128, 8, 0, 1, 3, 17, 45, 93, 91, 128, 8, 0, 1, 3, 17, 45, 93, 91, 128, 8]
+*/
+
+const filterValue = 10
+const filterHigher = true
+const filterThis = [0, 1, 3, 17, 45, 93, 91, 128, 8, 0, 1, 3, 17, 45, 93, 91, 128, 8, 'shazbat', 0, 1, 3, 17, 45, 93, 91, 128, 8, 0, 1, 3, 17, 45, 93, 91, 128, 8]
+
+console.log(
+  filterHigher ?
+    filterThis.filter(x => x > filterValue) :
+    filterThis.filter(x => x < filterValue)
+)
