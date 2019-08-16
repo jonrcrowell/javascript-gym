9d508211be597f502a9dfb44c144c1adfc128611
Jon Crowell <jon.crowell@stonegiantstudio.com>
2019-08-15 23:26:30 -0500

    Refactor insertion sort -- still needs a little work

--- exercises/jsfunctions/f24e-insertion-sort-in-place.js
+++ exercises/jsfunctions/f24e-insertion-sort-in-place.js
@@ -8,41 +8,26 @@ https://www.w3resource.com/javascript-exercises/javascript-functions-exercises.p
     Get the next element in the array and insert it in the correct order in the new array.
 */
 
-const sortMe = [1, 2, 5, 6, 7, 8, 9, 4, 17, 3, 0]
+const sortMe = [1, 2, 93, 147, 22, 0, 99, 5, 6, 7, 8, -9, 4, 17]
 //              [9, 1, 17, 4]
-//             [1, 2, 5, 6, 7, 8, 9, 4, 17]
-debugger;
+//             [1, 2, 93, 147, 22, 0, -9, 5, 6, 7, 8, 9, 4, 17]
 
-for (let i = 1; i < sortMe.length; i++) {
-    // 1
-    const el = sortMe[i]
-    if (i === 1) {
-        if (el < sortMe[0]) {
-            swapUs(sortMe, 0, 1)
-            continue;
+const sortedOut = insertionSort(sortMe)
+console.log(sortedOut)
+
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        const temp = arr[i];
+        let j = 0;
+
+        for (j = i - 1; j >= 0 && (arr[j] > temp); j--) {
+            // shift the number
+            arr[j + 1] = arr[j]
         }
+        //Insert the copied number at the correct position
+        //in sorted part. 
+        arr[j + 1] = temp;
     }
-
-    sortMe.splice(i, 1)
-
-    for (let j = 0; j <= i; j++) {
-        if (j === 0 && el < sortMe[0]) {
-            sortMe.unshift(el);
-            break;
-        }
-        if (j === i - 1 && el > sortMe[j]) {
-            sortMe.splice(j + 1, 0, el)
-            break;
-        }
-        if (el > sortMe[j] && el < sortMe[j + 1]) {
-            sortMe.splice(j + 1, 0, el)
-            break;
-        }
-    }
+    return arr;
 }
 
-function swapUs(array, i, j) {
-    return [array[i], array[j]] = [array[j], array[i]]
-}
-
-console.log(sortMe)
