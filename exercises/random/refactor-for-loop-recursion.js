68272623b522f2ebcef4a537f3ea675aaf7291fe
Jon Crowell <jon.crowell@stonegiantstudio.com>
2019-08-23 08:11:30 -0500

    Add for loop refactored to use recursion

---
+++ exercises/random/refactor-for-loop-recursion.js
@@ -0,0 +1,20 @@
+function counterFor() {
+  for(let i = 0; i <= 10; i++) {
+    console.log(i)
+  }
+}
+
+counterFor();
+
+function counterRecursively(n) {
+  console.log(n);
+  // we need at least one base case and one recursion case
+
+  // this is our base case
+  if(n >= 10) return;
+
+  // this is our recursion case
+  return counterRecursively(n+1)
+}
+
+counterRecursively(0);
