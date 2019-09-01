862a62896f7373349a2b87aa6dd9d520fdd39223
Jon Crowell <jon.crowell@stonegiantstudio.com>
2019-08-31 22:52:50 -0500

    Add two to one array challenge

---
+++ exercises/random/two2one.js
@@ -0,0 +1,23 @@
+/*
+Take 2 strings s1 and s2 including only letters from ato z. 
+Return a new sorted string, the longest possible, containing distinct letters,
+
+each taken only once - coming from s1 or s2.
+Examples:
+a = "xyaabbbccccdefww"
+b = "xxxxyyyyabklmopq"
+longest(a, b) -> "abcdefklmopqwxy"
+
+a = "abcdefghijklmnopqrstuvwxyz"
+longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+*/
+
+const a = "xyaabbbccccdefww";
+const b = "xxxxyyyyabklmopq";
+console.log(longest(a, b));
+
+function longest(s1, s2) {
+  return Array.from(new Set([...s1, ...s2]))
+    .sort()
+    .join("");
+}
