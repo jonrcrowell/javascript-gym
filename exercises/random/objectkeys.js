dba2b6be4b4595b65e7b13bbf967a67c735bd5ba
Jon Crowell <jon.crowell@stonegiantstudio.com>
2019-09-02 14:53:04 -0500

    Continue key study

--- exercises/random/objectkeys.js
+++ exercises/random/objectkeys.js
@@ -27,3 +27,14 @@ const myMap = new Map()
 for (const key of myMap.keys()) {
   console.log(key);
 }
+
+// spread map to an array
+const mapArrayKeys = [...myMap.keys()];
+console.log(`mapArrayKeys is ${mapArrayKeys}`);
+const mapArrayValues = [...myMap.values()];
+console.log(`mapArrayValues is ${mapArrayValues}`);
+
+// iterate through the entries of a map
+for (const entry of myMap.entries()) {
+  console.log(`Entry! ${entry}`);
+}
