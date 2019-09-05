754233740d0c63b0690edcf6d98d55e1dc5f19dd
Jon Crowell <jon.crowell@stonegiantstudio.com>
2019-09-05 08:39:00 -0500

    Add multi-step letter counter

--- exercises/jsfunctions/f6-longest-word.js
+++ exercises/jsfunctions/f6-longest-word.js
@@ -13,15 +13,18 @@ printThis("Current Exercise: jsfunction-6.js");
     Expected Output : 'Development'
   */
 
-const input = prompt("Enter a list of words or a sentence. I'll tell you the longest word.")
+const input = prompt(
+  "Enter a list of words or a sentence. I'll tell you the longest word."
+);
 
-const getLongestWord = function (str) {
-  const words = str.replace(/[^a-z]/gi, ' ').split(' ')  // include 0-9 [^a-z0-9] if I want to allow numbers to be counted
+const getLongestWord = function(str) {
+  // include 0-9 [^a-z0-9] if I want to allow numbers to be counted
   // gi makes the regex search global and case-insensitive so I don't need to worry about capital letters
-  const maxLength = words.map(x => x.length).reduce((x, y) => x > y ? x : y)
-  const longestWord = words.filter(x => x.length === maxLength)
+  const words = str.replace(/[^a-z]/gi, " ").split(" ");
+  const maxLength = words.map(x => x.length).reduce((x, y) => (x > y ? x : y));
+  const longestWord = words.filter(x => x.length === maxLength);
   // if we have more than one word of the max length, return all of them
-  return longestWord.length === 1 ? longestWord[0] : longestWord
-}
+  return longestWord.length === 1 ? longestWord[0] : longestWord;
+};
 
 console.log(getLongestWord(input));
---
+++ exercises/random/letterCounting.js
@@ -0,0 +1,43 @@
+/*
+Take a string str and return a string that is made up of the number of 
+occurances of each english letter in str, followed by that letter. 
+The string shouldn't contain zeros; leave them out.
+
+An empty string, or one with no letters, should return an empty string.
+
+Ignore all case.
+
+str will never be null.
+
+For Example:
+
+Kata.StringLetterCount("This is a test sentence.") == "1a1c4e1h2i2n4s4t"
+Kata.StringLetterCount("") == ""
+Kata.StringLetterCount("555") == ""
+*/
+
+const countMe = "555";
+
+console.log(stringLetterCount(countMe));
+
+function stringLetterCount(string) {
+  const letters = string
+    .replace(/[^a-z]/gi, "") // keep only letters
+    .toLowerCase()
+    .split("")
+    .sort();
+
+  // object to count occurrences
+  const counts = {};
+  const incrementLetterCount = x => (counts[x] = counts[x] ? counts[x] + 1 : 1);
+  letters.map(incrementLetterCount);
+
+  // map over object to create return value
+  let counted = [];
+  for (const key of Object.keys(counts)) {
+    counted.push(counts[key]);
+    counted.push(key);
+  }
+  // return
+  return counted.join("");
+}
