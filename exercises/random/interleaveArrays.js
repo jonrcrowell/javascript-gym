209ea36c0f4ba7fcb22fb733362f71d5a45229fa
Jon Crowell <jon.crowell@stonegiantstudio.com>
2019-09-07 09:55:50 -0500

    Add much cleaner approach using flatMap() and also map() then flat()

--- exercises/random/interleaveArrays.js
+++ exercises/random/interleaveArrays.js
@@ -9,7 +9,7 @@ eg. [1,2,3] + ['a','b','c'] = [1, 'a', 2, 'b', 3, 'c']
 const arr1 = [1, 2, 3];
 const arr2 = ["a", "b", "c"];
 
-console.log(mashArrays(arr1, arr2));
+console.log(mfMashArrays(arr1, arr2));
 
 function mashArrays(array1, array2) {
   let mashed = [];
@@ -18,3 +18,25 @@ function mashArrays(array1, array2) {
   }
   return mashed;
 }
+
+// much more elegant to use flatMap()
+function fMashArrays(array1, array2) {
+  return array1.flatMap((x, i) => [x, array2[i]]);
+}
+
+// which is similar to using map() and then flat()
+function mfMashArrays(array1, array2) {
+  return array1.map((x, i) => [x, array2[i]]).flat();
+}
+
+/*
+const locations = ["Austin", "New York", "San Francisco"];
+locations.forEach(location => {
+  doStuff();
+  doSomeOtherStuff();
+  // ...
+  // ...
+  // ...
+  dispatch(location);
+});
+*/
