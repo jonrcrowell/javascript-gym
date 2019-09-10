76a65fc6d39fe249512fbd82c4bba9970516a434
Jon Crowell <jon.crowell@stonegiantstudio.com>
2019-09-09 23:41:03 -0500

    Add meeting invite list parser

--- exercises/random/sortOutMeeting.js
+++ exercises/random/sortOutMeeting.js
@@ -17,16 +17,48 @@ const inviteList =
   "Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill";
 
 console.log(meeting(inviteList));
+console.log(meeting2(inviteList));
 
 function meeting(invited) {
-  const meeting = {};
-  return invited
+  let sortedList = "";
+  invited
     .toUpperCase()
     .split(";")
+    .map(x => x.split(":"))
     .map(x => {
-      return x
+      return {
+        firstName: x[0],
+        lastName: x[1],
+        inviteName: `(${x[1]}, ${x[0]})`
+      };
+    })
+    .sort(function(name1, name2) {
+      // sort by last name
+      if (name1.lastName > name2.lastName) return 1;
+      if (name1.lastName < name2.lastName) return -1;
+      // sort by first name
+      if (name1.firstName > name2.firstName) return 1;
+      if (name1.firstName < name2.firstName) return -1;
+    })
+    .map(x => {
+      sortedList = `${sortedList}${x.inviteName}`;
+    });
+
+  return sortedList;
+}
+
+// much terser functional answer
+function meeting2(s) {
+  let string = s
+    .toUpperCase()
+    .split(";")
+    .map(x =>
+      x
         .split(":")
         .reverse()
-        .join(",");
-    });
+        .join(", ")
+    )
+    .sort() // of course! I don't need to sort by both last and first name
+    .join(")(");
+  return "(" + string + ")";
 }
