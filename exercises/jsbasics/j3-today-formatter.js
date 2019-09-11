bd68a491e6576935dca19da707c4a002379c83e8
Jon Crowell <jon.crowell@stonegiantstudio.com>
2019-09-11 15:01:55 -0500

    Refactor today formatter

--- exercises/jsbasics/j3-today-formatter.js
+++ exercises/jsbasics/j3-today-formatter.js
@@ -1,16 +1,3 @@
-function printThis(input) {
-  document.write(`${input} <br><br>`);
-}
+const today = new Date().toISOString().split("T")[0]; //yyyy-mm-dd  2019-09-18
 
-printThis("Current Exercise: jsbasic-3.js");
-
-/* 
-  https://www.w3resource.com/javascript-exercises/javascript-basic-exercises.php
-  
-  Write a JavaScript program to get the current date.  Go to the editor
-  Expected Output : mm-dd-yyyy, mm/dd/yyyy or dd-mm-yyyy, dd/mm/yyyy
-*/
-
-const today = new Date().toISOString().split("T")[0];
-
-printThis(today);
+console.log(today);
