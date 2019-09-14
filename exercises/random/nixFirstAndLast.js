fc0b1c8361823d4eb7543a1755f38c57e24e1515
Jon Crowell <jon.crowell@stonegiantstudio.com>
2019-09-14 08:57:28 -0500

    Keep only slice version of remove first and last letter

--- exercises/random/nixFirstAndLast.js
+++ exercises/random/nixFirstAndLast.js
@@ -2,14 +2,8 @@ const words = "Your mother was a hamster";
 
 console.log(sliceFirstAndLast(words));
 
-function nixFirstAndLast(string) {
-  const pruned = string.split("");
-  pruned.shift();
-  pruned.pop();
-  return pruned.join("");
-}
-
 function sliceFirstAndLast(string) {
   // negative indexes count backwards from the end of the string
+  // note that array.slice() works the same way
   return string.slice(1, -1);
 }
