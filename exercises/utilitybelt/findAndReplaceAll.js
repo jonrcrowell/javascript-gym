7c4b249f7208af6fec8ee677a496dc0ecb49beeb
Jon Crowell <jon.crowell@stonegiantstudio.com>
2019-09-16 15:52:32 -0500

    Add explanation for my future self

--- exercises/utilitybelt/findAndReplaceAll.js
+++ exercises/utilitybelt/findAndReplaceAll.js
@@ -1,3 +1,15 @@
+/*
+RegExp can be set up with the new keyword or with slashes
+If you need to create the RegExp at run time, use the new version
+If you know the pattern at design time, you can use the slashes
+For Example
+string.replace(/5/, "S")
+or
+const matchMe = "er"
+const regex = new RegExp(matchMe, "g")  arguments are pattern and flags
+string.replace(regex, "erz")  arguments are the regex and the text to replace the matches with
+*/
+
 const input =
   "Your mother was a hamster. And your father smelt of elderberries.";
 const find = "er";
