bf0048dca8df1ae598ae26d24c10f2a79e99095c
Jon Crowell <jon.crowell@stonegiantstudio.com>
2019-09-16 15:46:48 -0500

    Add regexp version of find and replace all

--- exercises/utilitybelt/findAndReplaceAll.js
+++ exercises/utilitybelt/findAndReplaceAll.js
@@ -3,8 +3,14 @@ const input =
 const find = "er";
 const replace = "!!";
 
-console.log(findAndReplaceAll(input, find, replace));
+console.log(findReplaceRegex(input, find, replace));
 
 function findAndReplaceAll(input, find, replace) {
   return input.split(find).join(replace);
+}
+
+function findReplaceRegex(input, find, replace) {
+  const regex = new RegExp(find, "g");
+  let replaced = input.replace(regex, `${replace}`);
+  return replaced;
 }
