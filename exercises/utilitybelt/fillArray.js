703ea69556c70c4b4a8e31aa226fb233c13ae6a7
Jon Crowell <jon.crowell@stonegiantstudio.com>
2019-09-18 07:40:14 -0500

    Add comment to utility for filling an array

--- exercises/utilitybelt/fillArray.js
+++ exercises/utilitybelt/fillArray.js
@@ -1,7 +1,8 @@
 function fillArrayThrough(number) {
   return number < 0
     ? "Please pass in a positive number."
-    : [...Array(number + 1).keys()];
+    : [...Array(number + 1).keys()]; // if I want the array to start at 1
+  // append .slice(1) to the end of the array
 }
 
 console.log(fillArrayThrough(20));
