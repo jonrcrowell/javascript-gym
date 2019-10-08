2e89e6dd65deb82f5a2ccd8c89a2a5ef7c9f6199
Jon Crowell <jon.crowell@stonegiantstudio.com>
2019-10-07 20:06:28 -0500

    Add a few examples to the regexp test

--- exercises/random/regexpTest.js
+++ exercises/random/regexpTest.js
@@ -10,3 +10,9 @@ const testThis = "7";
 const result = str => /^[0-9]$/.test(str); // the ^ and $ limit this to a single character, not sure exactly how this works
 
 console.log(result(testThis));
+
+const digit = string => /^[0-9]$/.test(string);
+
+console.log(digit("Porsche")); // false
+console.log(digit("1")); // true
+console.log(digit("23")); // false
