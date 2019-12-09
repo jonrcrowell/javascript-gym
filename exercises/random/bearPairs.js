eea126b20199735f0b3b729c1317f07b86ec6ef6
Jon Crowell <jon.crowell@stonegiantstudio.com>
2019-12-09 13:56:11 -0600

    Add bear pairs exercise

---
+++ exercises/random/bearPairs.js
@@ -0,0 +1,26 @@
+/*
+Test.describe("Example tests",_=>{
+Test.assertSimilar(bears(7, '8j8mBliB8gimjB8B8jlB'), ["B8B8B8",false]);
+Test.assertSimilar(bears(3, '88Bifk8hB8BB8BBBB888chl8BhBfd'), ["8BB8B8B88B",true]); 
+Test.assertSimilar(bears(8, '8'), ["",false]); 
+*/
+
+const bearString = "i8BBkBdBBBBBc8Bh888B8afhBBf8k8 ";
+
+console.log(bearPairCount(5, bearString));
+
+function bearPairCount(count, population) {
+  let pairs = "";
+  for (let i = 0; i < population.length; i++) {
+    if (population[i] === "8" && population[i + 1] === "B") {
+      pairs += "8B";
+      i++;
+    } else if (population[i] === "B" && population[i + 1] === "8") {
+      pairs += "B8";
+      i++;
+    }
+  }
+  return [pairs, pairs.length / 2 >= count];
+}
+
+// '8BB8B8B8'
