2eb0daf238c0e9198bbd2a52345da047868684c9
Jon Crowell <jon.crowell@stonegiantstudio.com>
2019-12-16 09:25:54 -0600

    Decode an array of numbers into a string

---
+++ exercises/random/toStringEncoder.js
@@ -0,0 +1,59 @@
+/*
+Given an array of numbers, you must return a string. 
+:"T",mbers correspond to the letters of the alphabet in reverse order: a=26, z=1 etc. 
+You should also account for '!', '?' and ' ' that are represented by '27', '28' and '29' respectively.
+(switcher(['24', '12', '23', '22', '4', '26', '9', '8']), 'codewars');
+(switcher(['25','7','8','4','14','23','8','25','23','29','16','16','4']), 'btswmdsbd kkw'); 
+(switcher(['4', '24']), 'wc'); 
+*/
+
+const lookup = {
+  26: "a",
+  25: "b",
+  24: "c",
+  23: "d",
+  22: "e",
+  21: "f",
+  20: "g",
+  19: "h",
+  18: "i",
+  17: "j",
+  16: "k",
+  15: "l",
+  14: "m",
+  13: "n",
+  12: "o",
+  11: "p",
+  10: "q",
+  9: "r",
+  8: "s",
+  7: "t",
+  6: "u",
+  5: "v",
+  4: "w",
+  3: "x",
+  2: "y",
+  1: "z",
+  27: "!",
+  28: "?",
+  29: " "
+};
+
+const switcher = numArray => numArray.map(x => lookup[x]).join("");
+console.log(
+  switcher([
+    "25",
+    "7",
+    "8",
+    "4",
+    "14",
+    "23",
+    "8",
+    "25",
+    "23",
+    "29",
+    "16",
+    "16",
+    "4"
+  ])
+);
