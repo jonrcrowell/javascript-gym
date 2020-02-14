4902b0c090e3578982b1397651b34d4e5c9c0a21
Jon Crowell <jon.crowell@stonegiantstudio.com>
2020-02-14 08:08:14 -0600

    :white_check_mark: Export common js module for calcAverage test

--- exercises/random/calcAverage.js
+++ exercises/random/calcAverage.js
@@ -1,6 +1,6 @@
 /*
 It's the academic year's end, fateful moment of your school report. The averages must be calculated. 
-All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+All the students come to you and entreat you to calculate their average for them. Easy! You just need to write a script.
 
 Return the average of the given array rounded down to its nearest integer.
 
@@ -14,3 +14,5 @@ function calcAverages(grades) {
 }
 
 console.log(calcAverages(grades));
+
+module.exports = calcAverages;
