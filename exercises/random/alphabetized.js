18c759502df218c9f1e60e44c4c9ce911aeb6988
Jon Crowell <jon.crowell@stonegiantstudio.com>
2020-02-15 23:59:36 -0600

    :construction: Add test for alphabetizing chars

--- exercises/random/alphabetized.js
+++ exercises/random/alphabetized.js
@@ -1,10 +1,12 @@
 /*
-Re-order the characters of a string, so that they are concatenated into a new string in "case-insensitively-alphabetical-order-of-appearance" order. Whitespace and punctuation shall simply be removed!
+Re-order the characters of a string, so that they are concatenated into 
+a new string in "case-insensitively-alphabetical-order-of-appearance" order. 
+Whitespace and punctuation shall simply be removed!
 
 The input is restricted to contain no numerals and only words containing the english alphabet letters.
 */
 
-console.log(alphabetized("The Holy Bible_-,()@*&#%`^+=$'")); // "BbeehHilloTy"
+console.log(alphabetized("ChickEn Soup for the Soul")); // "BbeehHilloTy"
 
 function alphabetized(string) {
   return string
@@ -13,3 +15,5 @@ function alphabetized(string) {
     .sort((a, b) => a.localeCompare(b))
     .join("");
 }
+
+module.exports = alphabetized;
