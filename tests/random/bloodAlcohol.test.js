671e49b030b56248d58c777459fc22bae9918fc8
Jon Crowell <jon.crowell@stonegiantstudio.com>
2020-02-28 08:41:12 -0600

    :white_check_mark: Fix bloodAlcoholContent tests
    Wallaby isn't working correctly unfortunately

--- tests/random/bloodAlcohol.test.js
+++ tests/random/bloodAlcohol.test.js
@@ -1,12 +1,22 @@
-const bloodAlcoholContent = require("../../exercises/random/bloodAlcoholContent");
+const bloodAlcoholContent = require("../../exercises/random/bloodAlcohol");
+
+/*
+Test.assertEquals(bloodAlcoholContent({ounces:20, abv:0.4}, 100,'female', 2), 0.2414);
+*/
 
 it(`calculates blood alcohol content for males`, () => {
-  expect(bloodAlcoholContent({ ounces: 12.5, abv: 0.4 }, 190, "male", 1)).toBe(
-    0.0837
-  );
+  expect(
+    bloodAlcoholContent({ ounces: 12.5, abv: 0.4 }, 190, "male", 1)
+  ).toBeCloseTo(0.0837);
+  expect(
+    bloodAlcoholContent({ ounces: 50, abv: 0.14 }, 250, "male", 3)
+  ).toBeCloseTo(0.0601);
 });
 it(`calculates blood alcohol content for females`, () => {
   expect(
     bloodAlcoholContent({ ounces: 180, abv: 0.05 }, 160, "female", 1)
-  ).toBe(0.1758);
+  ).toBeCloseTo(0.1758);
+  expect(
+    bloodAlcoholContent({ ounces: 20, abv: 0.4 }, 100, "female", 2)
+  ).toBeCloseTo(0.2414);
 });
