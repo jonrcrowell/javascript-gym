dd33e1dce60a9a33742731010f91b73f53f79937
Jon Crowell <jon.crowell@stonegiantstudio.com>
2020-03-16 08:57:18 -0500

    :white_check_mark: Add more leap year tests

--- tests/jsbasics/isLeapYear.test.js
+++ tests/jsbasics/isLeapYear.test.js
@@ -1,5 +1,23 @@
 const isLeapYear = require("../../exercises/jsbasics/j6-isLeapYear");
 
 it(`detects a leap year for a year cleanly divisible by 4`, () => {
+  expect(isLeapYear(2004)).toBe(true);
+});
+
+it(`knows that years that aren't divisible by 4 aren't leap years`, () => {
+  expect(isLeapYear(2001)).toBe(false);
+  expect(isLeapYear(2002)).toBe(false);
+  expect(isLeapYear(2003)).toBe(false);
+  expect(isLeapYear(1697)).toBe(false);
+});
+
+it(`knows that years divisible by 100 but not 400 are not leap years`, () => {
+  expect(isLeapYear(1700)).toBe(false);
+  expect(isLeapYear(1800)).toBe(false);
+  expect(isLeapYear(1900)).toBe(false);
+});
+
+it(`knows that years divisible by both 100 and 400 are leap years`, () => {
+  expect(isLeapYear(1600)).toBe(true);
   expect(isLeapYear(2000)).toBe(true);
 });
